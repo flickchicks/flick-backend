c221fb5d9e9b79e53fa8ab0e6a6f20837ed0347e
asz33@cornell.edu <asz33@cornell.edu>
2020-05-20 16:59:34 -0700

    Add specific field

--- src/flick/flickaccount/admin.py
+++ src/flick/flickaccount/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import FlickAccount
 
+# Register your models here.
 admin.site.register(FlickAccount)
 
-# Register your models here.
 
--- src/flick/flickaccount/models.py
+++ src/flick/flickaccount/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class FlickAccount(models.Model):
-    username = models.TextField()
-    name = models.TextField()
+    username = models.CharField(max_length=30, unique=True) # max_length required for char field
+    name = models.CharField(max_length=60)
     profile_pic = models.TextField()
     phone_number = models.TextField()
     social_id_token_type = models.TextField()
