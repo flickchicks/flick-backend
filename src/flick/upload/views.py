4c730d7f87b28c701329e934d9b1f1b275e05592
asz33@cornell.edu <asz33@cornell.edu>
2020-05-24 19:26:45 -0700

    Get salt of image

--- src/flick/upload/views.py
+++ src/flick/upload/views.py
@@ -9,7 +9,7 @@ from api.generics import generics
 
 from PIL import Image
 from io import BytesIO
-import base64, json, re
+import base64, json, random, re, string
 
 
 # Create your views here.
@@ -19,19 +19,28 @@ class UploadImage(generics.CreateAPIView):
 
     # client is expected to send a base64 string
     def post(self, request):
-        print(f"{request.user}")
         data = json.loads(request.body)
 
         if not 'image' in data:
             return Response({'error': 'no image in request.'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
+            # remove header of base64 string
             image_data = base64.b64decode(re.sub('^data:image/.+;base64,', '', data['image']))
 
+            # get PIL image
             image = Image.open(BytesIO(image_data))
 
-            if image.format.lower() not in ['jpeg', 'jpg', 'png', 'gif']:
+            image_format = image.format.lower()
+
+            if image_format not in ['jpeg', 'jpg', 'png', 'gif']:
                 return Response({'error': 'Image type not accepted. Must be jpeg, jpg, png, or gif.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            salt = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
+            print(salt)
+
+            # save the image as {salt}.png in src/flick/tmp
+            image.save(f'tmp/{salt}.{image_format}')
 
         except Exception as e:
             return Response({'error': f'{e}'}, status=status.HTTP_400_BAD_REQUEST)
