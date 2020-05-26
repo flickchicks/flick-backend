0c4e3169abbf577f4c538ced4351e020288d0152
asz33@cornell.edu <asz33@cornell.edu>
2020-05-25 21:43:18 -0700

    Add django preview for images in asset bundle

--- src/flick/asset/admin.py
+++ src/flick/asset/admin.py
@@ -5,13 +5,25 @@ from django.utils.safestring import mark_safe
 
 # Register your models here.
 class AssetBundleAdmin(admin.ModelAdmin):
+
+    # show all assets in this bundle in django admin
+    def preview(self, obj):
+        html = '<ul>'
+        for url in obj.asset_urls.values():
+            # href makes image clickable to where it's being hosted
+            html += f'<li><a href="{url}" target="_blank"><img src="{url}" width=128/></a></li>'
+        html += '</ul>'
+        return mark_safe(html)
+
     list_display = ['salt', 'kind']
+    readonly_fields = ('preview',)
 
 admin.site.register(AssetBundle, AssetBundleAdmin)
 
 class AssetAdmin(admin.ModelAdmin):
+    
+    # use mark safe to show as image, not as html, in django admin
     def preview(self, obj):
-        # use mark safe to show as image, not as html, in django admin
         return mark_safe(f'<img src="{obj.full_url}" width="100"/>')
 
     preview.allow_tags = True
