7d8b12c039feff54c667fe102929b6442fea8103
asz33@cornell.edu <asz33@cornell.edu>
2020-06-06 22:01:55 -0700

    Fix user view in flick auth

--- src/flick/flick_auth/urls.py
+++ src/flick/flick_auth/urls.py
@@ -1,11 +1,9 @@
 from django.urls import include, path
 
-from .views import LoginView, LogoutView, RegisterView
-from user.views import UserView
+from .views import LoginView, LogoutView, RegisterView, UserView
 
 urlpatterns = [
     path("me/", UserView.as_view(), name="user"),
-    # path('me/profile/', ProfileView.as_view(), name='profile'),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
