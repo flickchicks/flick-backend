12f8760968748ca9bc0ab9abba30a7e9d2ebdf15
Vivi Ye <viviyyw0418@gmail.com>
2020-07-03 16:33:18 -0400

    change username to id

--- src/flick/friend/views.py
+++ src/flick/friend/views.py
@@ -31,7 +31,7 @@ class FriendList(APIView):
     permission_classes = api_settings.CONSUMER_PERMISSIONS
 
     def get(self, request, format=None):
-        friends = [User.objects.get(pk=friend.pk) for friend in Friend.objects.friends(user=request.user)]
+        friends = [User.objects.get(id=friend.id) for friend in Friend.objects.friends(user=request.user)]
         serializer = FriendUserSerializer(friends, many=True)
         return success_response(serializer.data)
 
@@ -51,9 +51,9 @@ class FriendRequestListAndCreate(generics.ListCreateAPIView):
     def post(self, request, format=None):
         data = json.loads(request.body)
         friend_requests = []
-        for username in data.get("usernames"):
+        for friend_id in data.get("user_ids"):
             try:
-                user = User.objects.get(username=username)
+                user = User.objects.get(id=friend_id)
                 friend_requests.append(Friend.objects.add_friend(request.user, user))
             except Exception as e:
                 print(str(e))
@@ -78,11 +78,11 @@ class FriendAcceptListAndCreate(generics.ListCreateAPIView):
     def post(self, request, format=None):
         data = json.loads(request.body)
         friends_accepted = []
-        for username in data.get("usernames"):
+        for friend_id in data.get("user_ids"):
             try:
-                friend = User.objects.get(username=username)
-                user_pk = request.user.pk
-                friend_request = FriendshipRequest.objects.get(from_user=friend.pk, to_user=user_pk)
+                friend = User.objects.get(id=friend_id)
+                user_id = request.user.id
+                friend_request = FriendshipRequest.objects.get(from_user=friend.id, to_user=user_id)
                 friend_request.accept()
                 friends_accepted.append(friend_request)
             except Exception as e:
@@ -109,10 +109,10 @@ class FriendRejectListAndCreate(generics.ListCreateAPIView):
     def post(self, request, format=None):
         data = json.loads(request.body)
         friends_rejected = []
-        for username in data.get("usernames"):
-            friend = User.objects.get(username=username)
-            user_pk = request.user.pk
-            friend_request = FriendshipRequest.objects.get(from_user=friend.pk, to_user=user_pk)
+        for friend_id in data.get("user_ids"):
+            friend = User.objects.get(id=friend_id)
+            user_id = request.user.id
+            friend_request = FriendshipRequest.objects.get(from_user=friend.id, to_user=user_id)
             friend_request.reject()
             friends_rejected.append(friend_request)
 
@@ -131,9 +131,9 @@ class FriendRemoveListAndCreate(generics.ListCreateAPIView):
     def post(self, request, format=None):
         data = json.loads(request.body)
         friends_removed = []
-        for username in data.get("usernames"):
+        for friend_id in data.get("user_ids"):
             try:
-                friend = User.objects.get(username=username)
+                friend = User.objects.get(id=friend_id)
                 Friend.objects.remove_friend(request.user, friend)
                 friends_removed.append(friend)
             except Exception as e:
