0a9d512b10acbbd019924ecdcc1b990d47f60f1e
asz33@cornell.edu <asz33@cornell.edu>
2020-07-03 12:32:13 -0700

    Fix user register fields

--- src/flick/flick_auth/serializers.py
+++ src/flick/flick_auth/serializers.py
@@ -21,13 +21,22 @@ class ProfileRegisterSerializer(serializers.ModelSerializer):
 
 class UserRegisterSerializer(serializers.ModelSerializer):
 
-    profile = ProfileRegisterSerializer()
+    profile_pic = serializers.CharField(source="profile.profile_pic")
+    social_id_token = serializers.CharField(source="profile.social_id_token")
+    social_id_token_type = serializers.CharField(source="profile.social_id_token_type")
 
     class Meta:
         model = User
         # Django's REQUIRED_FIELDS tuple(User.REQUIRED_FIELDS) +
-        fields = (User.USERNAME_FIELD, "first_name", "last_name", "profile")
-        write_only_fields = ("first_name", "last_name", "profile")
+        fields = (
+            User.USERNAME_FIELD,
+            "first_name",
+            "last_name",
+            "profile_pic",
+            "social_id_token",
+            "social_id_token_type",
+        )
+        write_only_fields = fields
 
     def save(self, **kwargs):
         data = self.init_data if hasattr(self, "init_data") else self.initial_data
@@ -37,13 +46,13 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             "username": items["username"],
             "first_name": items["first_name"],
             "last_name": items["last_name"],
-            "password": items["profile.social_id_token"],
+            "password": items["social_id_token"],
         }
 
         profile_data = {
-            "profile_pic": items["profile.profile_pic"],
-            "social_id_token": items["profile.social_id_token"],
-            "social_id_token_type": items["profile.social_id_token_type"],
+            "profile_pic": items["profile_pic"],
+            "social_id_token": items["social_id_token"],
+            "social_id_token_type": items["social_id_token_type"],
             "role": "consumer",
         }
 
