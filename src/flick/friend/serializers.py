38b1943ec5269ad88c71ac22ca300b58ffcd9583
Vivi Ye <viviyyw0418@gmail.com>
2020-07-10 20:12:28 -0400

    change friend serializer

--- src/flick/friend/serializers.py
+++ src/flick/friend/serializers.py
@@ -7,27 +7,16 @@ from asset.serializers import AssetBundleDetailSerializer
 from user.models import Profile
 
 
-class FriendProfileSerializer(serializers.ModelSerializer):
-
-    profile_asset_bundle = AssetBundleDetailSerializer(read_only=True)
-
-    class Meta:
-        model = Profile
-        fields = ("bio", "profile_asset_bundle", "owner_lsts", "collab_lsts")
-        read_only_fields = ("id",)
-
-
 class FriendUserSerializer(serializers.ModelSerializer):
-    """
-    User Serializer
-    """
 
-    profile = FriendProfileSerializer(many=False)
+    bio = serializers.CharField(source="profile.bio")
+    profile_pic = AssetBundleDetailSerializer(source="profile.profile_asset_bundle")
 
     class Meta:
         model = User
-        fields = ("id", User.USERNAME_FIELD, "first_name", "last_name", "profile")
-        read_only_fields = fields
+        # Django's REQUIRED_FIELDS tuple(User.REQUIRED_FIELDS) +
+        fields = (User.USERNAME_FIELD, "id", "first_name", "last_name", "bio", "profile_pic")
+        write_only_fields = fields
 
 
 class FriendRequestSerializer(serializers.ModelSerializer):
