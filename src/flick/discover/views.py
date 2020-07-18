be82bacd495fda069b8927ff7588fd8cdf84ba82
olivialy524 <olivialy524@gmail.com>
2020-07-18 05:51:16 -0400

    discover by genre

--- src/flick/discover/views.py
+++ src/flick/discover/views.py
@@ -20,20 +20,15 @@ from show.models import Show
 from show.serializers import ShowSerializer
 from show.utils import API
 from tag.models import Tag
+from tag.serializers import TagSerializer
 from user.user_simple_serializers import UserSimpleSerializer
 
 
-# cache to store get_top_movie and search_movie_by_name (and tv and anime)
-# get_top_movie example: ("top", "movie"), movie_id
-# search_movie_by_name example: ("query", "movie"), movie_id
 local_cache = caches["local"]
 
 
 class DiscoverShow(APIView):
     shows = []
-    known_shows = []
-
-    # show_type can be "movie", "tv", "anime"
 
     def get_top_shows_by_type(self, show_type):
         top_shows = local_cache.get(("top", show_type))
@@ -50,10 +45,12 @@ class DiscoverShow(APIView):
         if is_anime:
             self.get_top_shows_by_type("anime")
 
-    def get_top_shows_by_type_and_genre(self, show_type, tag_id=None):
+    def get_top_shows_by_type_and_genre(self, show_type, tag_id):
         top_shows = local_cache.get(("top", show_type, tag_id))
         if not top_shows:
-            top_shows = API.get_top_show_info(show_type)
+            tag_data = TagSerializer(Tag.objects.get(id=tag_id)).data
+            ext_api_tag = tag_data.get("ext_api_id")
+            top_shows = API.get_top_show_info_by_genre(show_type, ext_api_tag)
             local_cache.set(("top", show_type, tag_id), top_shows)
         self.shows.extend(top_shows)
 
@@ -63,7 +60,7 @@ class DiscoverShow(APIView):
         if is_tv:
             self.get_top_shows_by_type_and_genre("tv", tag_id)
         if is_anime:
-            self.get_top_shows_by_type_and_genre("anime")
+            self.get_top_shows_by_type("anime")  # genre does not seem to be fully integrated in the anime api
 
     def get(self, request, *args, **kwargs):
         tag_id = request.query_params.get("tag_id", "")
@@ -76,7 +73,6 @@ class DiscoverShow(APIView):
         print(f"is_tv: {is_tv}")
 
         self.shows = []
-        self.known_shows = []
 
         if tag_id:
             self.get_top_shows_by_genre(is_movie, is_tv, is_anime, tag_id)
@@ -85,6 +81,5 @@ class DiscoverShow(APIView):
 
         serializer_data = []
         serializer_data.extend(API.create_show_objects(self.shows))
-        serializer_data.extend(self.known_shows)
 
         return success_response(serializer_data)
