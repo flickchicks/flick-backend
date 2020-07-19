bbf5e8ad4254a39dadb1a26c58b681dcdd03906d
olivialy524 <olivialy524@gmail.com>
2020-07-19 17:40:09 -0400

    refactor

--- src/flick/search/views.py
+++ src/flick/search/views.py
@@ -30,12 +30,12 @@ class Search(APIView):
         return ext_api_tags
 
     # show_type can be "movie", "tv", "anime"
-    def get_shows_by_type_and_query(self, query, show_type, source, tag_lst=[]):
-        show_ids = local_cache.get((query, show_type, tag_lst))
+    def get_shows_by_type_and_query(self, query, show_type, source, tags=[]):
+        show_ids = local_cache.get((query, show_type, tags))
         if not show_ids:
-            ext_api_tags = self.get_ext_api_tags_by_tag_ids(tag_lst)
+            ext_api_tags = self.get_ext_api_tags_by_tag_ids(tags)
             show_ids = API.search_show_ids_by_name(show_type, query, ext_api_tags)
-            local_cache.set((query, show_type, tag_lst), show_ids)
+            local_cache.set((query, show_type, tags), show_ids)
         for show_id in show_ids:
             known_show = Show.objects.filter(ext_api_id=show_id, ext_api_source=source)
             if known_show.exists():
@@ -46,11 +46,11 @@ class Search(APIView):
                 if show:
                     self.shows.append(show)
 
-    def get_shows_by_query(self, query, is_movie, is_tv, is_anime, tag_lst):
+    def get_shows_by_query(self, query, is_movie, is_tv, is_anime, tags):
         if is_movie:
-            self.get_shows_by_type_and_query(query, "movie", "tmdb", tag_lst)
+            self.get_shows_by_type_and_query(query, "movie", "tmdb", tags)
         if is_tv:
-            self.get_shows_by_type_and_query(query, "tv", "tmdb", tag_lst)
+            self.get_shows_by_type_and_query(query, "tv", "tmdb", tags)
         if is_anime:
             self.get_shows_by_type_and_query(query, "anime", "animelist")
 
@@ -67,8 +67,8 @@ class Search(APIView):
     def get(self, request, *args, **kwargs):
         query = request.query_params.get("query")
         print(f"query: {query}")
-        tag_lst = request.GET.getlist("tags", [])
-        print(f"tag_lst: {tag_lst}")
+        tags = request.query_params.getlist("tags", [])
+        print(f"tags: {tags}")
         is_anime = bool(request.query_params.get("is_anime", False))
         print(f"is_anime: {is_anime}")
         is_movie = bool(request.query_params.get("is_movie", False))
@@ -88,7 +88,7 @@ class Search(APIView):
         elif is_lst:
             return success_response(self.get_lsts_by_name(query))
         else:
-            self.get_shows_by_query(query, is_movie, is_tv, is_anime, tag_lst)
+            self.get_shows_by_query(query, is_movie, is_tv, is_anime, tags)
 
         serializer_data = []
         serializer_data.extend(API.create_show_objects(self.shows))
