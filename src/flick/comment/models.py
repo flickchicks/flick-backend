cb603218935ac9c9adc3ae358490598fcde2b215
olivialy524 <olivialy524@gmail.com>
2020-08-02 19:48:57 -0400

    change comment unique together

--- src/flick/comment/models.py
+++ src/flick/comment/models.py
@@ -14,7 +14,7 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ["created_at"]
-        unique_together = ["owner", "message", "is_spoiler"]
+        unique_together = ["owner", "message", "is_spoiler", "show"]
 
     def __str__(self):
         return self.message
