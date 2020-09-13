4d88391e5df5cff2050f6d76ab109349ff905c67
Yue Li <36410894+olivialy524@users.noreply.github.com>
2020-09-13 00:48:03 -0400

    Change discover to only have trending shows (#169)
    * change discover
    
    * change naming

---
+++ src/flick/discover/tests/test_discover.py
@@ -0,0 +1,22 @@
+import json
+
+from django.test import TransactionTestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+
+class DiscoverTests(TransactionTestCase):
+    DISCOVER_URL = reverse("discover")
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_search_show(self):
+        response = self.client.get(self.DISCOVER_URL, format="json")
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(content.get("success"))
+        data = content.get("data")
+        self.assertEqual(len(data["trending_movies"]), 5)
+        self.assertEqual(len(data["trending_tvs"]), 5)
+        self.assertEqual(len(data["trending_animes"]), 5)
