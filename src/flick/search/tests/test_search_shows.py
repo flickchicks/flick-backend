909a573628f4599935393bba617295f8c6ab7d7f
asz33@cornell.edu <asz33@cornell.edu>
2020-12-20 12:17:30 -0800

    Update tests

--- src/flick/search/tests/test_search_shows.py
+++ src/flick/search/tests/test_search_shows.py
@@ -40,11 +40,13 @@ class SearchShowsTests(TransactionTestCase):
 
     def test_search_show(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
-        data = {"is_movie": True, "query": "Maleficent"}
+        query = "Maleficent"
+        data = {"is_movie": True, "query": query}
         response = self.client.get(self.SEARCH_URL, data, format="json")
         content = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(content.get("success"))
+        self.assertEqual(content.get("query"), query)
         data = content.get("data")[0]
         self.assertIn("id", data)
         self.assertIn("title", data)
