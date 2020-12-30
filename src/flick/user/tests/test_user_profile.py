d1b3964b750b44e853b48790257de08ab539bbe4
asz33@cornell.edu <asz33@cornell.edu>
2020-12-30 15:40:11 -0800

    Add friend_status instead of is_friend

--- src/flick/user/tests/test_user_profile.py
+++ src/flick/user/tests/test_user_profile.py
@@ -17,6 +17,7 @@ class UserProfileTests(TestCase):
     USER_ID = 1
     FRIEND_ID = 2
     RANDO_ID = 3
+    USER_PROFILE_URL = reverse("user-profile", kwargs={"pk": USER_ID})
     FRIEND_PROFILE_URL = reverse("user-profile", kwargs={"pk": FRIEND_ID})
     RANDO_PROFILE_URL = reverse("user-profile", kwargs={"pk": RANDO_ID})
 
@@ -24,7 +25,7 @@ class UserProfileTests(TestCase):
         self.client = APIClient()
         self.user_token = self._create_user_and_login()
         self.friend_token = self._create_user_and_login()
-        # self.rando_token = self._create_user_and_login()
+        self.rando_token = self._create_user_and_login()
 
     def _create_user_and_login(self):
         """Returns the auth token."""
@@ -48,27 +49,66 @@ class UserProfileTests(TestCase):
         token = json.loads(response.content)["data"]["auth_token"]
         return token
 
-    def _create_friendship(self, user1, user2):
+    def _request_friendship(self, from_user, to_user):
         try:
-            Friend.objects.add_friend(user1, user2).accept()
+            return Friend.objects.add_friend(from_user, to_user)
         except AlreadyFriendsError:
             return
 
+    def _accept_friendship(self, friend_request):
+        try:
+            if friend_request:
+                friend_request.accept()
+        except AlreadyFriendsError:
+            return
+
+    # Known to fail when running `python manage.py test` likely due to concurrency
     def test_view_friend_profile(self):
-        self._create_friendship(user1=User.objects.get(id=self.USER_ID), user2=User.objects.get(id=self.FRIEND_ID))
+        from_user = User.objects.get(id=self.USER_ID)
+        to_user = User.objects.get(id=self.FRIEND_ID)
+        friend_request = self._request_friendship(from_user=from_user, to_user=to_user)
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         response = self.client.get(self.FRIEND_PROFILE_URL)
         content = json.loads(response.content)["data"]
         self.assertEqual(response.status_code, 200)
         self.assertEqual(content.get("id"), self.FRIEND_ID)
-        self.assertTrue(content.get("is_friend"))
+        self.assertEqual(content.get("friend_status"), "outgoing request")
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
+        response = self.client.get(self.USER_PROFILE_URL)
+        content = json.loads(response.content)["data"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content.get("id"), self.USER_ID)
+        self.assertEqual(content.get("friend_status"), "incoming request")
+
+        self._accept_friendship(friend_request)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
+        response = self.client.get(self.FRIEND_PROFILE_URL)
+        content = json.loads(response.content)["data"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content.get("id"), self.FRIEND_ID)
+        self.assertEqual(content.get("friend_status"), "friends")
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
+        response = self.client.get(self.USER_PROFILE_URL)
+        content = json.loads(response.content)["data"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content.get("id"), self.USER_ID)
+        self.assertEqual(content.get("friend_status"), "friends")
 
     # Known to fail when running `python manage.py test` likely due to concurrency
     def test_view_rando_profile(self):
-        Friend.objects.remove_friend(User.objects.get(id=self.USER_ID), User.objects.get(id=self.FRIEND_ID))
+        Friend.objects.remove_friend(User.objects.get(id=self.USER_ID), User.objects.get(id=self.RANDO_ID))
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
-        response = self.client.get(self.FRIEND_PROFILE_URL)
+        response = self.client.get(self.RANDO_PROFILE_URL)
         content = json.loads(response.content)["data"]
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(content.get("id"), self.FRIEND_ID)
-        self.assertFalse(content.get("is_friend"))
+        self.assertEqual(content.get("id"), self.RANDO_ID)
+        self.assertEqual(content.get("friend_status"), "not friends")
+
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.rando_token)
+        response = self.client.get(self.USER_PROFILE_URL)
+        content = json.loads(response.content)["data"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content.get("id"), self.USER_ID)
+        self.assertEqual(content.get("friend_status"), "not friends")
