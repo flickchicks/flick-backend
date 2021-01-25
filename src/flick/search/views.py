a1edd1780aa021edc426a0b44435aaef5c924271
Vivi Ye <viviyyw0418@gmail.com>
2021-01-25 16:08:50 -0500

    filter out default lists

--- src/flick/search/views.py
+++ src/flick/search/views.py
@@ -70,7 +70,9 @@ class Search(APIView):
         return serializer.data
 
     def get_lsts_by_name(self, query):
-        lsts = Lst.objects.filter(Q(name__icontains=query) & Q(is_private=False))
+        lsts = Lst.objects.filter(
+            Q(name__icontains=query) & Q(is_private=False) & ~Q(name="Saved") & ~Q(name="Watch Later")
+        )
         serializer = LstSerializer(lsts, many=True)
         return serializer.data
 
