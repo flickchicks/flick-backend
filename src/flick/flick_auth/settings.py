c1628bdd1c79dd2e147184772106b980e658dd90
asz33@cornell.edu <asz33@cornell.edu>
2021-02-02 19:28:36 -0500

    Restructure code

--- src/flick/flick_auth/settings.py
+++
@@ -1,2 +0,0 @@
-AUTH_PASSWORD_SALT = 'LtC66ubP'
-AUTH_TOKEN_AGE = 60*15 # 15 minutes 
---
+++ src/friend/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from friendship.models import Friend
+from friendship.models import FriendshipRequest
+
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    """
+    override registered modal Friend Request from django-friendship
+    """
+
+    list_display = ("from_user", "to_user", "created", "rejected")
+
+
+admin.site.unregister(FriendshipRequest)
+admin.site.register(FriendshipRequest, FriendRequestAdmin)
+
+
+class FriendAdmin(admin.ModelAdmin):
+    """
+    override registered modal Friend from django-friendship
+    """
+
+    list_display = ("from_user", "to_user", "created")
+
+
+admin.site.unregister(Friend)
+admin.site.register(Friend, FriendAdmin)
