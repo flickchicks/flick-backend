784352aa2fbf6821d1559bdaab30fcadc42e7053
alanna-zhou <asz33@cornell.edu>
2021-02-03 17:37:19 -0500

    Add voting to groups (#259)

--- src/group/tests/test_groups.py
+++ src/group/tests/test_groups.py
@@ -6,11 +6,7 @@ from rest_framework.test import APIClient
 
 
 class GroupTests(FlickTestCase):
-    GROUP_ID = 1
     GROUP_LIST_URL = reverse("group-list")
-    UPDATE_GROUP_URL = reverse("group-detail", kwargs={"pk": GROUP_ID})
-    ADD_TO_GROUP_URL = reverse("group-detail-add", kwargs={"pk": GROUP_ID})
-    REMOVE_FROM_GROUP_URL = reverse("group-detail-remove", kwargs={"pk": GROUP_ID})
 
     def setUp(self):
         self.client = APIClient()
@@ -18,6 +14,9 @@ class GroupTests(FlickTestCase):
         self.user2_token = self._create_user_and_login()
         self.user3_token = self._create_user_and_login()
         self.user4_token = self._create_user_and_login()
+        self._view_groups(num_groups=0)
+        self.GROUP_NAME = "dummy name"
+        self._create_group(name=self.GROUP_NAME, members=[2, 3])
 
     def _view_groups(self, num_groups):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
@@ -40,13 +39,21 @@ class GroupTests(FlickTestCase):
         self.assertEqual(len(data["members"]), len(members) + 1)
         return data
 
-    def _update_group(self, name="", members=[], shows=[], is_add=False, is_remove=False):
+    def _view_group_detail(self, user_token, group_id=1):
+        url = reverse("group-detail", kwargs={"pk": group_id})
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token)
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)["data"]
+        return data
+
+    def _update_group(self, group_id=1, name="", members=[], shows=[], is_add=False, is_remove=False):
         if is_add:
-            url = self.ADD_TO_GROUP_URL
+            url = reverse("group-detail-add", kwargs={"pk": group_id})
         elif is_remove:
-            url = self.REMOVE_FROM_GROUP_URL
+            url = reverse("group-detail-remove", kwargs={"pk": group_id})
         else:
-            url = self.UPDATE_GROUP_URL
+            url = reverse("group-detail", kwargs={"pk": group_id})
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         request_data = {
             "name": name,
@@ -58,21 +65,52 @@ class GroupTests(FlickTestCase):
         data = json.loads(response.content)["data"]
         return data
 
+    def _vote(self, user_token, group_id=1, show_id=1, vote_type="yes", assert_success=True):
+        url = reverse("group-vote-show", kwargs={"group_pk": group_id, "show_pk": show_id})
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token)
+        request_data = {"vote": vote_type}
+        response = self.client.post(url, request_data, format="json")
+        content = json.loads(response.content)
+        if assert_success:
+            self.assertEqual(response.status_code, 200)
+            return content["data"]
+        self.assertEqual(response.status_code, 404)
+        return content["error"]
+
+    def _view_results(self, user_token, group_id=1):
+        url = reverse("group-show-list", kwargs={"pk": group_id})
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token)
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)["data"]
+        return data
+
+    def _view_pending(self, user_token, group_id=1):
+        url = reverse("group-pending-list", kwargs={"pk": group_id})
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token)
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)["data"]
+        return data
+
+    def _clear(self, user_token, group_id=1):
+        url = reverse("group-clear-shows", kwargs={"pk": group_id})
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + user_token)
+        response = self.client.post(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)["data"]
+        return data
+
     def test_groups(self):
-        self._view_groups(num_groups=0)
-
-        group_name = "dummy name"
-        self._create_group(name=group_name, members=[2, 3])
-
+        # group was first created at setup
         self._view_groups(num_groups=1)
-
         new_group_name = "new dummy name"
         data = self._update_group(name=new_group_name)
         self.assertEqual(data.get("name"), new_group_name)
 
         new_member_id = 4
         new_show_id = self._create_show().id
-        data = self._update_group(name=group_name, members=[new_member_id], shows=[new_show_id], is_add=True)
+        data = self._update_group(name=self.GROUP_NAME, members=[new_member_id], shows=[new_show_id], is_add=True)
         # add to group does not modify group name
         self.assertEqual(data.get("name"), new_group_name)
         self.assertEqual(len(data.get("members")), 4)
@@ -82,4 +120,60 @@ class GroupTests(FlickTestCase):
         # remove from group does not modify group name
         self.assertEqual(data.get("name"), new_group_name)
         self.assertEqual(len(data.get("members")), 3)
+        self.assertEqual(len(data.get("shows")), 0)
+
+    def test_vote_pending_and_clear(self):
+        new_show_id = self._create_show().id
+        new_show_id2 = self._create_show().id
+        data = self._update_group(name=self.GROUP_NAME, shows=[new_show_id, new_show_id2], is_add=True)
+        self.assertEqual(len(data.get("shows")), 2)
+
+        # haven't voted yet, should have 2 pending shows
+        data = self._view_pending(self.user_token)
+        self.assertEqual(len(data.get("shows")), 2)
+
+        # only one vote so far
+        data = self._vote(user_token=self.user_token, show_id=new_show_id, vote_type="yes")
+        self.assertEqual(len(data), 1)
+
+        # view results
+        data = self._view_results(self.user_token)
+        self.assertEqual(data.get("num_members"), 3)
+        self.assertEqual(data.get("num_voted"), 1)
+        self.assertTrue(data.get("user_voted"))
+        self.assertEqual(len(data.get("results")), 2)
+        self.assertEqual(data.get("results")[0]["num_yes_votes"], 1)
+        self.assertEqual(data.get("results")[0]["num_maybe_votes"], 0)
+        self.assertEqual(data.get("results")[0]["num_no_votes"], 0)
+
+        # other members should have `user_voted` as False
+        data = self._view_results(self.user2_token)
+        self.assertEqual(data.get("num_members"), 3)
+        self.assertEqual(data.get("num_voted"), 1)
+        self.assertFalse(data.get("user_voted"))
+        self.assertEqual(len(data.get("results")), 2)
+        self.assertEqual(data.get("results")[0]["num_yes_votes"], 1)
+        self.assertEqual(data.get("results")[0]["num_maybe_votes"], 0)
+        self.assertEqual(data.get("results")[0]["num_no_votes"], 0)
+
+        # can't vote more than once with the same vote_type
+        error = self._vote(
+            user_token=self.user_token, group_id=1, show_id=new_show_id, vote_type="yes", assert_success=False
+        )
+        self.assertEqual(error, "Already voted yes!")
+
+        # changing vote to "maybe" doesn't result in duplicate votes
+        data = self._vote(user_token=self.user_token, group_id=1, show_id=new_show_id, vote_type="maybe")
+        self.assertEqual(len(data), 1)
+
+        # pending one more show
+        data = self._view_pending(self.user_token)
+        self.assertEqual(len(data.get("shows")), 1)
+
+        # viewing group should still have two shows
+        data = self._view_group_detail(self.user_token)
+        self.assertEqual(len(data.get("shows")), 2)
+
+        # any member can clear shows
+        data = self._clear(self.user2_token)
         self.assertEqual(len(data.get("shows")), 0)
--- src/group/views.py
+++ src/group/views.py
@@ -1,11 +1,18 @@
+import datetime
 import json
 from user.models import Profile
 
 from api import settings as api_settings
 from api.utils import failure_response
 from api.utils import success_response
+import pytz
 from rest_framework import generics
 from show.models import Show
+from show.serializers import GroupShowSerializer
+from show.serializers import ShowSerializer
+from vote.models import Vote
+from vote.models import VoteType
+from vote.serializers import VoteSerializer
 
 from .models import Group
 from .serializers import GroupSerializer
@@ -137,4 +144,119 @@ class GroupDetailRemove(generics.GenericAPIView):
             group.shows.remove(show)
         group.save()
         serializer = self.serializer_class(group)
+        return success_response(serializer.data)
+
+
+class GroupClearShows(generics.GenericAPIView):
+    serializer_class = GroupSerializer
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def post(self, request, pk):
+        """Clear all shows in a group by id."""
+        if not Profile.objects.filter(user=request.user).exists():
+            return failure_response(f"No user to be found with id of {request.user.id}.")
+        profile = Profile.objects.get(user=request.user)
+        if not profile.groups.filter(id=pk).exists():
+            return failure_response(f"User does not belong to group with id {pk} or group with id {pk} does not exist.")
+        group = profile.groups.get(id=pk)
+        group.shows.clear()
+        group.votes.clear()
+        serializer = self.serializer_class(group)
+        return success_response(serializer.data)
+
+
+class GroupShowList(generics.GenericAPIView):
+    serializer_class = GroupShowSerializer
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def get(self, request, pk):
+        """View show results in a group by id."""
+        if not Profile.objects.filter(user=request.user).exists():
+            return failure_response(f"No user to be found with id of {request.user.id}.")
+        profile = Profile.objects.get(user=request.user)
+        if not profile.groups.filter(id=pk).exists():
+            return failure_response(f"User does not belong to group with id {pk} or group with id {pk} does not exist.")
+        group = profile.groups.get(id=pk)
+        num_members = group.members.count()
+        num_voted = 0
+        for member in group.members.all():
+            if group.votes.filter(voter=member).exists():
+                num_voted += 1
+        user_voted = group.votes.filter(voter=profile).count() > 0
+        serializer = self.serializer_class(group.shows.all(), context={"group_id": group.id}, many=True)
+        data = {
+            "num_members": num_members,
+            "num_voted": num_voted,
+            "user_voted": user_voted,
+            "results": serializer.data,
+        }
+        return success_response(data)
+
+
+class GroupPendingList(generics.GenericAPIView):
+    serializer_class = ShowSerializer
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def get(self, request, pk):
+        """Get the list of the user's pending (not yet voted) shows in a group by id."""
+        # timestamp needed for frontend to not show old results if we return them slower than
+        # the users vote
+        request_timestamp = datetime.datetime.now(tz=pytz.utc)
+        if not Profile.objects.filter(user=request.user).exists():
+            return failure_response(f"No user to be found with id of {request.user.id}.")
+        profile = Profile.objects.get(user=request.user)
+        if not profile.groups.filter(id=pk).exists():
+            return failure_response(f"User does not belong to group with id {pk} or group with id {pk} does not exist.")
+        group = profile.groups.get(id=pk)
+        voted_show_ids = group.votes.filter(voter=profile).values_list("show", flat=True)
+        all_show_ids = group.shows.all().values_list("id", flat=True)
+        pending_show_ids = all_show_ids.difference(voted_show_ids)
+        serializer = self.serializer_class(
+            group.shows.filter(id__in=pending_show_ids), context={"request": self.request}, many=True
+        )
+        data = {"request_timestamp": request_timestamp, "shows": serializer.data}
+        return success_response(data)
+
+
+class GroupVoteShow(generics.GenericAPIView):
+    serializer_class = VoteSerializer
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def post(self, request, group_pk, show_pk):
+        """Vote for a show in a group by id."""
+        if not Profile.objects.filter(user=request.user).exists():
+            return failure_response(f"No user to be found with id of {request.user.id}.")
+        profile = Profile.objects.get(user=request.user)
+        if not profile.groups.filter(id=group_pk).exists():
+            return failure_response(
+                f"User does not belong to group with id {group_pk} or group with id {group_pk} does not exist."
+            )
+        group = profile.groups.get(id=group_pk)
+        if not group.shows.filter(id=show_pk).exists():
+            return failure_response(f"Show with id {show_pk} does not belong to group with id {group_pk}.")
+        show = group.shows.get(id=show_pk)
+        data = json.loads(request.body)
+        vote_str = data.get("vote")
+        if vote_str == "yes":
+            choice = VoteType.YES
+        elif vote_str == "maybe":
+            choice = VoteType.MAYBE
+        elif vote_str == "no":
+            choice = VoteType.NO
+        else:
+            return failure_response("Votes can only be 'yes', 'no', or 'maybe'.")
+        vote_exists = group.votes.filter(voter=profile, choice=choice, show=show)
+        if vote_exists:
+            return failure_response(f"Already voted {vote_str}!")
+        old_vote = group.votes.filter(voter=profile, show=show)
+        if old_vote:
+            vote = group.votes.get(voter=profile, show=show)
+            vote.choice = choice
+            vote.save()
+            group.save()
+        else:
+            vote = Vote.objects.create(voter=profile, choice=choice, show=show)
+            group.votes.add(vote)
+            group.save()
+        serializer = self.serializer_class(group.votes.all(), many=True)
         return success_response(serializer.data)
