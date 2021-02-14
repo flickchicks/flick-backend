eac099e4c7dbf769259ba5960c45cd5ae4b5f372
olivialy524 <olivialy524@gmail.com>
2021-02-14 15:06:31 -0500

    refactoring

--- src/recommend/views.py
+++ src/recommend/views.py
@@ -1,7 +1,6 @@
 from random import sample
 
 from api import settings as api_settings
-from api.utils import failure_response
 from api.utils import success_response
 from django.core.cache import caches
 from lst.models import Lst
@@ -13,9 +12,9 @@ from show.tmdb import flicktmdb
 local_cache = caches["local"]
 
 
-# Create your views here.
 class LstRecommendView(generics.GenericAPIView):
     permission_classes = api_settings.CONSUMER_PERMISSIONS
+    api = flicktmdb()
 
     def get(self, request, pk):
         """
@@ -23,8 +22,6 @@ class LstRecommendView(generics.GenericAPIView):
         If the request user is a collaborator or an owner or is looking at a list that is public,
         then the list will be returned.
         """
-        if not Lst.objects.filter(pk=pk):
-            return failure_response(f"List of id {pk} does not exist.")
         lst = Lst.objects.get(pk=pk)
         shows = lst.shows.filter(ext_api_source="tmdb")
 
@@ -32,7 +29,7 @@ class LstRecommendView(generics.GenericAPIView):
         for show in shows:
             similar = local_cache.get((show.id, "similar"))
             if not similar:
-                similar = flicktmdb().get_similar_shows(show.ext_api_id, show.is_tv)
+                similar = self.api.get_similar_shows(show.ext_api_id, show.is_tv)
                 local_cache.set((show.id, "similar"), similar)
             rec_shows.extend(similar)
 
