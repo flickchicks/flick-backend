8edfdde4692922275c0af39023782c3da22827c9
asz33@cornell.edu <asz33@cornell.edu>
2021-02-13 20:46:14 -0500

    Add testing

--- src/suggestion/tests/test_private_suggestion.py
+++ src/suggestion/tests/test_private_suggestion.py
@@ -42,20 +42,14 @@ class PrivateSuggestionTests(FlickTestCase):
     def _suggest_show(self, token, suggest_data):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
         response = self.client.post(self.SUGGEST_URL, suggest_data, format="json")
-        data = json.loads(response.content)["data"]
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data), 2)
-        self.assertEqual(data[-1]["show"]["id"], suggest_data.get("show_id"))
-        self.assertEqual(data[-1]["message"], suggest_data.get("message"))
+        self.assertTrue(json.loads(response.content)["success"])
 
     def _check_suggestion(self, friend_token, suggest_data):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + friend_token)
         response = self.client.get(self.SUGGESTIONS_URL)
-        data = json.loads(response.content)["data"]
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data), 1)
-        self.assertEqual(data[-1]["show"]["id"], suggest_data.get("show_id"))
-        self.assertEqual(data[-1]["message"], suggest_data.get("message"))
+        self.assertTrue(json.loads(response.content)["success"])
 
     def _check_me_has_num_notifs(self, friend_token, num_notifs):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + friend_token)
@@ -72,10 +66,8 @@ class PrivateSuggestionTests(FlickTestCase):
         self._check_me_has_num_notifs(self.friend1_token, 1)
         self._check_me_has_num_notifs(self.friend2_token, 1)
 
-        # test suggestion already sent failure
+        # test suggestion already sent, used to be failure but now just success
         suggest_data = {"users": [2, 3], "message": "Great film", "show_id": self.show.pk}
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_token)
         response = self.client.post(self.SUGGEST_URL, suggest_data, format="json")
-        self.assertEqual(response.status_code, 404)
-        error = json.loads(response.content)["error"]
-        self.assertEqual(error, "Suggestion has already been sent!")
+        self.assertEqual(response.status_code, 200)
