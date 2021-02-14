92a2fe4ad27719db6970dfe3266faadd5e5f4c08
alanna-zhou <asz33@cornell.edu>
2021-02-14 15:30:35 -0500

    Fix group notif bug (#272)

--- src/group/tasks.py
+++ src/group/tasks.py
@@ -60,15 +60,20 @@ def create_new_group_notif(profile_id, group_id, member_ids):
     from_user = User.objects.get(profile__id=profile_id)
     from_profile = Profile.objects.get(id=profile_id)
     for user_id in member_ids:
+        # don't notify the group creator
+        if user_id == from_user.id:
+            continue
         try:
+            to_user = User.objects.get(id=user_id)
+            to_profile = User.objects.get(user__id=user_id)
             notif = Notification()
             notif.notif_type = "group_invite"
             notif.from_user = from_profile
-            notif.to_user = Profile.objects.get(user__id=user_id)
+            notif.to_user = to_profile
             notif.group = Group.objects.get(id=group_id)
             notif.save()
-            ios_devices = APNSDevice.objects.filter(user=from_user, active=True)
-            android_devices = GCMDevice.objects.filter(user=from_user, active=True)
+            ios_devices = APNSDevice.objects.filter(user=to_user, active=True)
+            android_devices = GCMDevice.objects.filter(user=to_user, active=True)
             message_title = f"{from_user.username} added you to a group"
             message_body = "Decide what to watch together 😊"
             ios_devices.send_message(message={"title": message_title, "body": message_body})
