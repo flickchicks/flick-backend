c53110803e2112f24119f1e432d1e59262b613ab
olivialy524 <olivialy524@gmail.com>
2021-02-20 11:27:32 -0500

    fix test case

--- src/like/tests/test_like_and_unlike.py
+++ src/like/tests/test_like_and_unlike.py
@@ -1,9 +1,12 @@
 import json
+from user.models import Profile
 
 from api.tests import FlickTestCase
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APIClient
 from show.models import Show
+from suggestion.models import PrivateSuggestion
 
 
 class LikeTests(FlickTestCase):
@@ -12,14 +15,18 @@ class LikeTests(FlickTestCase):
         self.show = self._create_show()
         self.user_token = self._create_user_and_login()
         self.friend_token = self._create_user_and_login()
+        self.user = User.objects.get(id=1)
+        self.friend = User.objects.get(id=2)
+        self._create_friendship(user1=self.user, user2=self.friend)
+        self.suggestion_pk = self._suggest_show()
         self.SHOW_DETAIL_URL = reverse("show-detail", kwargs={"pk": self.show.pk})
         self.SUGGEST_URL = reverse("private-suggestion")
         self.SUGGESTION_URL = reverse("private-suggestion-list")
         self.NOTIFICATION_URL = reverse("notif-list")
-        # self.suggestion_pk = self._suggest_show(self.user_token)
         self.comment_pk = self._comment_show(self.user_token)
         self.LIKE_COMMENT_URL = reverse("like-comment", kwargs={"pk": self.comment_pk})
         self.LIKE_LST_URL = reverse("like-list", kwargs={"pk": 1})
+        self.LIKE_SUGGESTION_URL = reverse("like-private-suggestion", kwargs={"pk": self.suggestion_pk})
 
     def _create_show(self):
         show = Show()
@@ -48,24 +55,14 @@ class LikeTests(FlickTestCase):
         self.assertEqual(comment["message"], comment_data.get("comment").get("message"))
         return comment["id"]
 
-    def _view_suggestions(self):
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
-        response = self.client.get(self.SUGGESTION_URL)
-        data = json.loads(response.content)["data"]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data), 1)
-        suggestion = data[0]
-        self.assertEqual(suggestion["from_user"]["id"], 2)
-        self.assertEqual(suggestion["to_user"]["id"], 1)
-        self.suggestion_pk = suggestion["id"]
-        self.LIKE_SUGGESTION_URL = reverse("like-private-suggestion", kwargs={"pk": self.suggestion_pk})
-
-    def _suggest_show(self, token):
-        suggest_data = {"users": [1, 2], "show": self.show.pk, "message": "great movie"}
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
-        response = self.client.post(self.SUGGEST_URL, suggest_data, format="json")
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(json.loads(response.content)["success"])
+    def _suggest_show(self):
+        pri_suggestion = PrivateSuggestion()
+        pri_suggestion.from_user = Profile.objects.get(user=self.user)
+        pri_suggestion.to_user = Profile.objects.get(user=self.friend)
+        pri_suggestion.show = self.show
+        pri_suggestion.message = "good show"
+        pri_suggestion.save()
+        return pri_suggestion.id
 
     def test_like_and_cancel(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
@@ -129,7 +126,6 @@ class LikeTests(FlickTestCase):
 
     def test_suggestion_like_and_cancel(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
-        self._view_suggestions()
 
         # test like
         response = self.client.post(self.LIKE_SUGGESTION_URL)
