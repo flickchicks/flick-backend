1e4da0740460ceb06c2eb689e9c4f18f5e3fcccc
olivialy524 <olivialy524@gmail.com>
2021-02-20 10:57:32 -0500

    change test case

--- src/like/tests/test_like_and_unlike.py
+++ src/like/tests/test_like_and_unlike.py
@@ -14,12 +14,12 @@ class LikeTests(FlickTestCase):
         self.friend_token = self._create_user_and_login()
         self.SHOW_DETAIL_URL = reverse("show-detail", kwargs={"pk": self.show.pk})
         self.SUGGEST_URL = reverse("private-suggestion")
-        self._suggest_show(self.user_token)
+        self.SUGGESTION_URL = reverse("private-suggestion-list")
+        self.NOTIFICATION_URL = reverse("notif-list")
+        # self.suggestion_pk = self._suggest_show(self.user_token)
         self.comment_pk = self._comment_show(self.user_token)
         self.LIKE_COMMENT_URL = reverse("like-comment", kwargs={"pk": self.comment_pk})
         self.LIKE_LST_URL = reverse("like-list", kwargs={"pk": 1})
-        self.LIKE_SUGGESTION_URL = reverse("like-private-suggestion", kwargs={"pk": 1})
-        self.NOTIFICATION_URL = reverse("notif-list")
 
     def _create_show(self):
         show = Show()
@@ -48,21 +48,24 @@ class LikeTests(FlickTestCase):
         self.assertEqual(comment["message"], comment_data.get("comment").get("message"))
         return comment["id"]
 
+    def _view_suggestions(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
+        response = self.client.get(self.SUGGESTION_URL)
+        data = json.loads(response.content)["data"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 1)
+        suggestion = data[0]
+        self.assertEqual(suggestion["from_user"]["id"], 2)
+        self.assertEqual(suggestion["to_user"]["id"], 1)
+        self.suggestion_pk = suggestion["id"]
+        self.LIKE_SUGGESTION_URL = reverse("like-private-suggestion", kwargs={"pk": self.suggestion_pk})
+
     def _suggest_show(self, token):
-        suggest_data = {"users": [], "show": self.show.pk, "message": "great movie"}
+        suggest_data = {"users": [1, 2], "show": self.show.pk, "message": "great movie"}
         self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
         response = self.client.post(self.SUGGEST_URL, suggest_data, format="json")
         self.assertEqual(response.status_code, 200)
         self.assertTrue(json.loads(response.content)["success"])
-
-        response = self.client.get(self.NOTIFICATION_URL)
-        data = json.loads(response.content)["data"]
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(data), 1)
-        notification = data[0]
-        self.assertEqual(notification["from_user"]["id"], 2)
-        self.assertEqual(notification["to_user"]["id"], 1)
-        self.assertEqual(notification["notif_type"], "comment_like")
 
     def test_like_and_cancel(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
@@ -126,6 +129,7 @@ class LikeTests(FlickTestCase):
 
     def test_suggestion_like_and_cancel(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.friend_token)
+        self._view_suggestions()
 
         # test like
         response = self.client.post(self.LIKE_SUGGESTION_URL)
