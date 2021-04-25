b840153403378ffa8c23e36de2b9082f07dd4776
asz33@cornell.edu <asz33@cornell.edu>
2021-04-24 20:37:07 -0400

    400

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(rpi100.ddnsfree.com www.rpi100.ddnsfree.com)
+domains=(rpi400.ddnsfree.com www.rpi400.ddnsfree.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="asz33@cornell.edu" # Adding a valid address is strongly recommended
