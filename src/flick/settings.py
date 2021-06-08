309d5ec100fdeaa9028c73dced64eff03c9b9686
asz33@cornell.edu <asz33@cornell.edu>
2021-06-08 17:23:46 -0400

    Refactor settings

--- src/flick/settings.py
+++ src/flick/settings.py
@@ -212,11 +212,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = "/static"
 
-TEMP_DIR = "/tmp"
-
-# S3_BUCKET = "flick"
-# S3_BASE_URL = f"https://{S3_BUCKET}.s3-us-west-1.amazonaws.com/"
-
 SPACES_ACCESS_KEY_ID = os.getenv("SPACES_ACCESS_KEY_ID")
 SPACES_SECRET_ACCESS_KEY = os.getenv("SPACES_SECRET_ACCESS_KEY")
 SPACES_BUCKET_NAME = os.getenv("SPACES_BUCKET_NAME")
