b3375e76996ef90bb12644276145f0d29645235a
alanna-zhou <asz33@cornell.edu>
2022-02-22 23:14:01 -0500

    Get all reactions for one episode with `public` or `friends` filter (#374)
    * Add view all reactions for an episode
    
    * Fix error message

--- src/api/urls.py
+++ src/api/urls.py
@@ -45,6 +45,7 @@ from notification.views import NotificationList
 from notification.views import NotificationTest
 from reaction.views import ReactionAdd
 from reaction.views import ReactionRemove
+from reaction.views import ReactionsForEpisode
 from read.views import ReadView
 from recommend.views import LstRecommendView
 from rest_framework import routers
@@ -102,6 +103,7 @@ urlpatterns = [
     path("lsts/<int:pk>/like/", LstLikeView.as_view(), name="like-list"),
     path("me/", UserView.as_view(), name="me"),
     path("media/image/", UploadImage.as_view(), name="upload"),
+    path("reactions/", ReactionsForEpisode.as_view(), name="reactions-for-ep"),
     path("reactions/add/", ReactionAdd.as_view(), name="add-reaction"),
     path("reactions/<int:pk>/remove/", ReactionRemove.as_view(), name="remove-reaction"),
     path("search/", Search.as_view(), name="search"),
--- src/reaction/views.py
+++ src/reaction/views.py
@@ -1,14 +1,45 @@
 import json
-from user.models import Profile
 
 from api import settings as api_settings
 from api.utils import failure_response
 from api.utils import success_response
 from episode_detail.models import EpisodeDetail
+from friendship.models import Friend
 from reaction.serializers import ReactionSerializer
 from rest_framework import generics
 
 from .models import Reaction
+from .models import VisibilityChoice
+
+
+class ReactionsForEpisode(generics.GenericAPIView):
+    queryset = Reaction.objects.all()
+    serializer_class = ReactionSerializer
+
+    permission_classes = api_settings.CONSUMER_PERMISSIONS
+
+    def post(self, request):
+        """
+        Get all (relevant) reactions for one episode
+        {
+            "episode_id": <>,
+            "filter_by":  "public" or "friends"
+        }
+        """
+        data = json.loads(request.body)
+        episode_id = data.get("episode_id")
+        filter_by = data.get("filter_by").lower()
+        if filter_by == VisibilityChoice.PUBLIC:
+            reactions = Reaction.objects.filter(episode__id=episode_id, visibility=VisibilityChoice.PUBLIC)
+        elif filter_by == VisibilityChoice.FRIENDS:
+            friends = [friend.profile for friend in Friend.objects.friends(user=request.user)]
+            reactions = Reaction.objects.filter(
+                episode__id=episode_id, visibility=VisibilityChoice.FRIENDS, author__in=friends
+            )
+        else:
+            return failure_response(f"filter_by must be '{VisibilityChoice.PUBLIC}' or '{VisibilityChoice.FRIENDS}'!")
+        serializer = self.serializer_class(reactions, many=True)
+        return success_response(serializer.data)
 
 
 class ReactionAdd(generics.GenericAPIView):
@@ -26,13 +57,12 @@ class ReactionAdd(generics.GenericAPIView):
             "text": <>,
         }
         """
-        profile = Profile.objects.get(user=request.user)
         data = json.loads(request.body)
         episode_id = data.get("episode_id")
         if not EpisodeDetail.objects.filter(id=episode_id).exists():
             return failure_response(f"Episode with id {episode_id} does not exist!")
         text = data.get("text")
-        reaction = Reaction(episode=EpisodeDetail.objects.get(id=episode_id), text=text, author=profile)
+        reaction = Reaction(episode=EpisodeDetail.objects.get(id=episode_id), text=text, author=request.user.profile)
         reaction.save()
         serializer = self.serializer_class(reaction)
         return success_response(serializer.data)
