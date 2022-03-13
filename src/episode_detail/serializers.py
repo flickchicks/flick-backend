9e2fdba02a074e4319a85aa4b56f1c9d3bb270b8
Vivi Ye <viviyyw0418@gmail.com>
2022-03-13 19:45:03 -0400

    show one reaction from each user

--- src/episode_detail/serializers.py
+++ src/episode_detail/serializers.py
@@ -14,8 +14,20 @@ class EpisodeDetailShortenedReactionsSerializer(serializers.ModelSerializer):
     reactions = serializers.SerializerMethodField("get_subset_of_reactions")
 
     def get_subset_of_reactions(self, obj):
-        reactions = obj.reactions.filter(visibility=VisibilityChoice.PUBLIC)[:10]
-        return ReactionSerializer(instance=reactions, many=True).data
+        reactions = obj.reactions.filter(visibility=VisibilityChoice.PUBLIC).order_by("author")
+        if reactions.count() < 10:
+            return ReactionSerializer(instance=reactions, many=True).data
+        else:
+            distinct_reactions, reaction_ids, author_ids = [], [], []
+            for reaction in reactions:
+                if reaction.author.id not in author_ids:
+                    author_ids.append(reaction.author.id)
+                    reaction_ids.append(reaction.id)
+                    distinct_reactions.append(reaction)
+            if len(distinct_reactions) < 10:
+                additional_reactions = reactions.exclude(id__in=reaction_ids)[: 10 - len(distinct_reactions)]
+
+            return ReactionSerializer(instance=distinct_reactions + list(additional_reactions), many=True).data
 
     class Meta:
         model = EpisodeDetail
