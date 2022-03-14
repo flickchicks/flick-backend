a8228e9fe3cc1a4be78865b78cc7a8c842266e25
Vivi Ye <viviyyw0418@gmail.com>
2022-03-13 20:24:30 -0400

    change filter method

--- src/episode_detail/serializers.py
+++ src/episode_detail/serializers.py
@@ -18,12 +18,11 @@ class EpisodeDetailShortenedReactionsSerializer(serializers.ModelSerializer):
         if reactions.count() < 10:
             return ReactionSerializer(instance=reactions, many=True).data
         else:
-            distinct_reactions, reaction_ids, author_ids = [], [], []
-            for reaction in reactions:
-                if reaction.author.id not in author_ids:
-                    author_ids.append(reaction.author.id)
-                    reaction_ids.append(reaction.id)
-                    distinct_reactions.append(reaction)
+            authors = reactions.values_list("author", flat=True).distinct()[:10]
+            distinct_reactions, reaction_ids = [], []
+            for author in authors:
+                reaction = reactions.filter(author=author).first()
+                distinct_reactions.append(reaction), reaction_ids.append(reaction.id)
             if len(distinct_reactions) < 10:
                 additional_reactions = reactions.exclude(id__in=reaction_ids)[: 10 - len(distinct_reactions)]
 
